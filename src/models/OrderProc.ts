b34d34a2d80343892396c0d9037a64df75daeec1
mahhis <vasiukovich03@gmail.com>
2024-05-06 22:47:30 +0300

    fix some

--- src/models/OrderProc.ts
+++ src/models/OrderProc.ts
@@ -1,48 +1,50 @@
-import { getModelForClass } from "@typegoose/typegoose";
-import { Order } from "./Order";
-import { User } from "./User";
-
+import { Order } from 'src/models/Order'
+import { User } from 'src/models/User'
+import { getModelForClass } from '@typegoose/typegoose'
 
 export const OrderModel = getModelForClass(Order)
 
-
 export function findOrCreateOrder(user: User, id: number) {
-    return OrderModel.findOneAndUpdate(
-      { user, id },
-      {},
-      {
-        upsert: true,
-        new: true,
-      }
-    )
-  }
-  
-  export async function findLastAddedOrder(user: User) {
-    return await OrderModel.findOne({ user: user }).sort({
-      createdAt: -1,
-    })
-  }
-  
-  export async function deleteOrder(ordere: Order) {
-    return await OrderModel.findOneAndDelete({ id: ordere.id })
-  }
-  
-  export async function getOrders(user: User, countryFrom: string | undefined, countryTo: string | undefined) {
-    return await OrderModel.find({ 
-      user: { $ne: user }, 
-      countryTo: countryFrom, 
-      countryFrom: countryTo, 
-      ready: true
-    });
-  }
+  return OrderModel.findOneAndUpdate(
+    { user, id },
+    {},
+    {
+      upsert: true,
+      new: true,
+    }
+  )
+}
 
-  export async function findOrderById(orderId: any) {
-    return await OrderModel.findById(orderId).exec();
-  }
+export async function findLastAddedOrder(user: User) {
+  return await OrderModel.findOne({ user: user }).sort({
+    createdAt: -1,
+  })
+}
 
-  export async function getOrdersByUser(user: User) {
-    return await OrderModel.find({ 
-        user: user,
-        ready: true
-    });
+export async function deleteOrder(ordere: Order) {
+  return await OrderModel.findOneAndDelete({ id: ordere.id })
+}
+
+export async function getOrders(
+  user: User,
+  countryFrom: string | undefined,
+  countryTo: string | undefined
+) {
+  return await OrderModel.find({
+    user: { $ne: user },
+    countryTo,
+    countryFrom,
+    ready: true,
+  })
+}
+
+export async function findOrderById(orderId: any) {
+  return await OrderModel.findById(orderId).exec()
+}
+
+export async function getOrdersByUser(user: User) {
+  return await OrderModel.find({
+    user: user,
+    ready: true,
+  })
 }
